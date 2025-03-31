a387c113791755283100d9ba4a83803f4453b65d
Techie-Mahesh <maheshTech1205@gmail.com>
2025-03-31 21:18:54 +0530

    Time Interval

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { Typography } from "@mui/material";
-import TimeInterval2 from "./TimeInterval2";
 import Images from "./Images";
 import Form from "./form";
 import ImperativeHook from "./useImperativeHook";
 import Form1 from "./Lists";
 import CounterInterval from "./Interval/CounterInterval";
 import Timer from "./Interval/Timer";
+import TimeInterval from "./Interval/TimeInterval";
 
 function App() {
   return (
     <div className="App">
       <CounterInterval />
-      <TimeInterval2 />
       <Timer />
+      <TimeInterval />
       {/* <Images /> */}
       {/* <Form /> */}
       <Typography variant="h4">Time Interval</Typography>
