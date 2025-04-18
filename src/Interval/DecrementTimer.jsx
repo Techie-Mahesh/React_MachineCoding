487239bf1d6757c849bc365d343ad732de7d041e
Techie-Mahesh <maheshTech1205@gmail.com>
2025-04-18 11:40:27 +0530

    Decrement Timer

---
+++ src/Interval/DecrementTimer.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import "./style.css";
+
+export default function DecrementTimer() {
+  const [timer, setTimer] = React.useState(10);
+  const intervalRef = React.useRef(null);
+
+  const handleClick = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    intervalRef.current = setInterval(() => {
+      setTimer(prev => {
+        if (prev > 0) {
+          return prev - 1;
+        } else {
+          clearInterval(intervalRef.current);
+          return 0;
+        }
+      });
+    }, 500);
+  };
+  const handleStop = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
+  useEffect(() => {
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello StackBlitz! {timer} </h1>
+      <button onClick={handleClick}>start</button>
+      <button onClick={handleStop}>stop</button>
+    </div>
+  );
+}
